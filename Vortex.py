1c3d2b7ab76c99a078c0a9f05501db6e60474bda
contralious <frantisek.cech103@gmail.com>
2025-12-04 21:14:21 +0100

    Update Vortex.py
    added something even sillier

--- Vortex.py
+++ Vortex.py
@@ -1,4 +1,7 @@
-# Copyright (c) 2025 contralious (GitHub) / c_tra (Discord). All rights reserved.
+# Copyright (c) 2025 contralious
+# Licensed under the GNU General Public License v3.0
+# See the LICENSE file for details.
+
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import messagebox
